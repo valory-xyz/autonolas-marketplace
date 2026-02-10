d65a2a166a7594f8fa340aac8b2229744a9ec2e2
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2026-02-10 14:53:15 +0000

    chore: deployment scripts for celo

---
+++ scripts/deployment/deploy_06_celo_balance_tracker_fixed_price_native.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+reset=$(tput sgr0)
+
+# Check if $1 is provided
+if [ -z "$1" ]; then
+  echo "${red}!!! Usage: $0 <network>${reset}"
+  echo "${red}Example: $0 eth_mainnet${reset}"
+  exit 1
+fi
+
+# check if the ETHERSCAN_API_KEY is set
+if [ -z "$ETHERSCAN_API_KEY" ]; then
+  echo "${red}!!! Set the ETHERSCAN_API_KEY environment variable${reset}"
+  exit 1
+fi
+
+# Get globals file
+globals="$(dirname "$0")/globals_$1.json"
+if [ ! -f $globals ]; then
+  echo "${red}!!! $globals is not found${reset}"
+  exit 0
+fi
+
+# Read variables using jq
+contractVerification=$(jq -r '.contractVerification' $globals)
+useLedger=$(jq -r '.useLedger' $globals)
+derivationPath=$(jq -r '.derivationPath' $globals)
+chainId=$(jq -r '.chainId' $globals)
+networkURL=$(jq -r '.networkURL' $globals)
+
+mechMarketplaceProxyAddress=$(jq -r '.mechMarketplaceProxyAddress' $globals)
+drainerAddress=$(jq -r '.drainerAddress' $globals)
+wrappedNativeTokenAddress=$(jq -r '.wrappedNativeTokenAddress' $globals)
+
+contractName="BalanceTrackerFixedPriceNativeCelo"
+contractPath="contracts/mechs/native/celo/$contractName.sol:$contractName"
+constructorArgs="$mechMarketplaceProxyAddress $drainerAddress $wrappedNativeTokenAddress"
+contractArgs="$contractPath --constructor-args $constructorArgs"
+
+# Get deployer based on the ledger flag
+if [ "$useLedger" == "true" ]; then
+  walletArgs="-l --mnemonic-derivation-path $derivationPath"
+  deployer=$(cast wallet address $walletArgs)
+else
+  echo "Using PRIVATE_KEY: ${PRIVATE_KEY:0:6}..."
+  walletArgs="--private-key $PRIVATE_KEY"
+  deployer=$(cast wallet address $walletArgs)
+fi
+
+# Deployment message
+echo "${green}Deploying from: $deployer${reset}"
+echo "RPC: $networkURL"
+echo "${green}Deployment of: $contractArgs${reset}"
+
+# Deploy the contract and capture the address
+execCmd="forge create --broadcast --rpc-url $networkURL $walletArgs $contractArgs"
+deploymentOutput=$($execCmd)
+balanceTrackerFixedPriceNativeAddress=$(echo "$deploymentOutput" | grep 'Deployed to:' | awk '{print $3}')
+
+# Get output length
+outputLength=${#balanceTrackerFixedPriceNativeAddress}
+
+# Check for the deployed address
+if [ $outputLength != 42 ]; then
+  echo "${red}!!! The contract was not deployed...${reset}"
+  exit 0
+fi
+
+# Write new deployed contract back into JSON
+echo "$(jq '. += {"balanceTrackerFixedPriceNativeAddress":"'$balanceTrackerFixedPriceNativeAddress'"}' $globals)" > $globals
+
+# Verify contract
+if [ "$contractVerification" == "true" ]; then
+  contractParams="$balanceTrackerFixedPriceNativeAddress $contractPath --constructor-args $(cast abi-encode "constructor(address,address,address)" $constructorArgs)"
+  echo "Verification contract params: $contractParams"
+
+  echo "${green}Verifying contract on Etherscan...${reset}"
+  forge verify-contract --chain-id "$chainId" --etherscan-api-key "$ETHERSCAN_API_KEY" $contractParams
+
+  blockscoutURL=$(jq -r '.blockscoutURL' $globals)
+  if [ "$blockscoutURL" != "null" ]; then
+    echo "${green}Verifying contract on Blockscout...${reset}"
+    forge verify-contract --verifier blockscout --verifier-url "$blockscoutURL/api" $contractParams
+  fi
+fi
+
+echo "${green}$contractName deployed at: $balanceTrackerFixedPriceNativeAddress${reset}"
